e6bacee26dc9a698ddd5ef9654a7bc9675cfd123
AndrewEvl <Lino220037@gmail.com>
2018-04-10 12:20:29 +0300

    almost done

--- src/main/java/by/login/controller/UserController.java
+++ src/main/java/by/login/controller/UserController.java
@@ -4,6 +4,7 @@ import by.login.entity.Status;
 import by.login.entity.User;
 import by.login.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -32,10 +33,16 @@ public class UserController {
         return new User();
     }
 
-    @ModelAttribute("id")
-    public Long id() {
-        return null;
-    }
+//    @ModelAttribute("id")
+//    public Long id() {
+//        return null;
+//    }
+
+    @Value("${error.massage}")
+    private String errorMassage;
+
+    @Value("${error.id}")
+    private String errorId;
 
     @GetMapping("/")
     public String saveUserGet() {
@@ -46,25 +53,33 @@ public class UserController {
     Method to upload avatar user on server.
      */
     @PostMapping("/")
-    public String saveUserPost(User user, @RequestParam("file") MultipartFile file) {
-        if (!file.isEmpty()) {
-            try {
-                byte[] bytes = file.getBytes();
-                File fileSave = new File(file.getOriginalFilename());
-                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(fileSave + ".jpeg"));
-                stream.write(bytes);
-                stream.close();
-                //Here you can specify the full path to the file
-                String absolutePath = fileSave.getPath();
-                user.setImage(absolutePath);
-            } catch (IOException e) {
-                e.printStackTrace();
+    public String saveUserPost(User user, Model model, @RequestParam("file") MultipartFile file) {
+        if (user.getFirstName() != null && user.getFirstName().length() > 0
+                && user.getLastName() != null && user.getLastName().length() > 0
+                && user.getMail() != null && user.getMail().length() > 0
+                && user.getPassword() != null && user.getPassword().length() > 0) {
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    File fileSave = new File(file.getOriginalFilename());
+                    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(fileSave + ".jpeg"));
+                    stream.write(bytes);
+                    stream.close();
+                    //Here you can specify the full path to the file
+                    String absolutePath = fileSave.getPath();
+                    user.setImage(absolutePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+            stub();
+            user.setStatusNow(Status.ONLINE);
+            userRepository.save(user);
+            model.addAttribute("id", "Your unique ID : " +  user.getId());
+            return "save-user-page";
         }
-        stub();
-        user.setStatusNow(Status.ONLINE);
-        userRepository.save(user);
-        return "redirect:/";
+        model.addAttribute("errorMassage", errorMassage);
+        return "save-user-page";
     }
 
     @GetMapping("/find-user")
@@ -74,11 +89,23 @@ public class UserController {
 
     @PostMapping("/find-user")
     public String findUserByIdPost(User user, Model model) {
-        stub();
-        User userById = userRepository.findById(user.getId()).get();
-        ID = user.getId();
-        model.addAttribute("user", userById);
-        return "redirect:/user-info";
+        try {
+            User userById = userRepository.findById(user.getId()).get();
+            if (userById.getFirstName() != null && userById.getFirstName().length() > 0
+                    && userById.getLastName() != null && userById.getLastName().length() > 0
+                    && userById.getMail() != null && userById.getMail().length() > 0
+                    && userById.getPassword() != null && userById.getPassword().length() > 0) {
+                ID = user.getId();
+                stub();
+                model.addAttribute("user", userById);
+                return "redirect:/user-info";
+            }
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+
+        model.addAttribute("errorId", errorId);
+        return "find-user";
     }
 
     /*
@@ -95,21 +122,15 @@ public class UserController {
         stub();
         model.addAttribute("user", user);
         model.addAttribute("id", ID);
+        //can not load an image. I guess there's something wrong with the path to the file.
         model.addAttribute("image", "/" + user.getImage());
         return "user-info";
     }
 
-    @GetMapping("/user-info{id}")
-    public String userInfoIdGet(@PathVariable("id") Long id, Model model) {
-        User user = userRepository.findById(id).get();
-        model.addAttribute("user", user);
-        return "user-info";
-    }
-
-    //Stub for 3 seconds.
-    private void stub (){
+    //Stub for 5 seconds.
+    private void stub() {
         try {
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
